fb0b6281a38455f6b80d722359a5a107e3f908d1
hanabeeeee <105402245+hanabeeeee@users.noreply.github.com>
2022-08-02 12:04:55 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,8 +2,20 @@
 $(function(){
     $('#toggle').click(function(){
         $('#main-header nav').toggleClass('on')
-    })
+    });
 
     // AOS 초기화
 	AOS.init();
-})
+
+    // 모달창
+    //열기
+    $('#main-section2 .youtube img').click(function(){
+        $('.modal').addClass('on')
+    });
+
+    //닫기
+    $('.box .btn-close').click(function(){
+        $('.modal').removeClass('on')
+    });
+
+}) //$
